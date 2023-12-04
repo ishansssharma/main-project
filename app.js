8136e9257ef3f0672a7dee09c14700737bd127ad
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2023-12-04 18:39:30 +0100

    Add javascript to the footer button

--- app.js
+++ app.js
@@ -30,3 +30,135 @@ function showPage(event) {
 
 
 }
+
+const slideshow = document.querySelector(".slideshow");
+const slides = slideshow.querySelectorAll(".slideshow-slide");
+const controls = slideshow.querySelectorAll(".slideshow__control-button");
+const counter = slideshow.querySelector(".slideshow__counter");
+const dots = slideshow.querySelectorAll(".slideshow__dot");
+
+let index = 0;
+
+const totalSlides = slides.length
+const lastIndex = slides.length;
+
+console.log(totalSlides);
+const setIndex = (newIndex) => {
+	index = newIndex;
+}
+
+const decreaseIndex = () => {
+	if (index > 0) {
+		index = index - 1;
+	} else {
+		index = lastIndex;
+	}
+};
+
+const increaseIndex = () => {
+	if (index < lastIndex) {
+		index = index + 1;
+	} else {
+		index = 1;
+	}
+}
+
+const renderSlideClass = () => {
+	slides.forEach(slide => {
+		slide.classList.remove("slideshow-slide--visible");
+	});
+	slides[index].classList.add("slideshow-slide--visible");
+}
+
+const renderCounter = () => {
+	counter.textContent = `${index} of ${totalSlides}`;
+}
+
+const switchSlide = (event) => {
+	const button = event.currentTarget;
+
+	if (button.dataset.direction === "previous") {
+		decreaseIndex();
+	}
+
+	if (button.dataset.direction === "next") {
+		increaseIndex();
+	}
+
+	if (button.dataset.index) {
+		setIndex(parseInt(button.dataset.index));
+	}
+	renderSlideClass();
+	renderCounter();
+}
+
+controls.forEach(button => {
+	button.addEventListener("click", switchSlide);
+});
+
+dots.forEach(button => {
+	button.addEventListener("click", () => {
+		const dotIndex = Array.from(dots).indexOf(button);
+		setIndex(dotIndex);
+		renderSlideClass();
+		renderCounter();
+	});
+});
+
+const filterButtons = document.querySelectorAll(".filter-button");
+const headers = document.querySelectorAll(".header");
+const headerContainer = document.querySelector(".header");
+
+let lastClickedFilterVia = null;
+let lastClickedFilterValue = null;
+
+const filterTheList = (event) => {
+	const currentButton = event.currentTarget;
+
+	let currentButtonFilterVia = currentButton.dataset.filterVia;
+
+	let currentButtonFilterValue = currentButton.dataset.filter;
+
+	if (currentButtonFilterValue === lastClickedFilterValue) {
+		currentButtonFilterValue = "*"
+	}
+
+	lastClickedFilterValue = currentButtonFilterVia;
+	lastClickedFilterValue = currentButtonFilterValue;
+
+	const filteredItems = [...headers].filter(item => {
+		if (lastClickedFilterValue === "*") {
+			return true;
+		} else {
+			return item.dataset[currentButtonFilterVia] === currentButtonFilterValue;
+		}
+	});
+
+	headerContainer.innerHTML = " ";
+
+	filterButtons.forEach(button => {
+		button.classList.remove("filter-button--active");
+	})
+
+	if (currentButtonFilterValue !== "*") {
+		currentButton.classList.add("filter-button--active");
+	}
+
+}
+
+filterButtons.forEach(filterButton => {
+	filterButton.addEventListener("click", filterTheList)
+});
+
+
+
+const toTheTopButton = document.querySelector(".tothetop");
+
+toTheTopButton.addEventListener("click", () => {
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "smooth",
+
+	});
+})
